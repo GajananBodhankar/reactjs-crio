88a7d993bd7190efa6cc08c3f3d7cd57e577408c
GajananBodhankar <bodhankargajanan99@gmail.com>
2024-01-19 13:37:37 +0530

    Made changes in id

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@ import Superhero from "./listandkeys/superhero.tsx";
 
 function App() {
   let [data, setData] = useState([
-    { id: "a2jdkjd", name: "Winter Soldier" },
-    { id: "jdkdjk1", name: "Iron Man" },
+    { id: "1", name: "Winter Soldier" },
+    { id: "2", name: "Iron Man" },
     { id: "b3dkdj2", name: "Black Widow" },
     { id: 10, name: "max" },
     { id: 11, name: "john" },

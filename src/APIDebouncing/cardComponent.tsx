3274d3f7e5f65ffc7d09e1d23446ba09d02e00ed
GajananBodhankar <bodhankargajanan99@gmail.com>
2024-02-06 14:59:58 +0530

    Made some configurations

--- src/APIDebouncing/cardComponent.tsx
+++ src/APIDebouncing/cardComponent.tsx
@@ -1,80 +1,24 @@
-import {
-  Box,
-  Button,
-  Card,
-  CardContent,
-  CardMedia,
-  CircularProgress,
-  InputAdornment,
-  Link,
-  OutlinedInput,
-  Rating,
-  Stack,
-  Typography,
-} from "@mui/material";
-import React, { useEffect } from "react";
-import SearchIcon from "@mui/icons-material/Search";
-import "./video.css";
-import axios from "axios";
-import { RemoveOutlined } from "@mui/icons-material";
-
-async function getData() {
-  let response = await axios.post("url", i, {
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  });
-  let data = await fetch("uel", {
-    method: "POST",
-    // body:JSON.stringify()
-    headers: {
-      "Content-type": "application/json",
-      "Authorization": `Bearer ${token}}`,
-    },
-  });
-}
-
-function CardComponent({ image, genre, title, release, videoLink }) {
+import { Box, Grid, Typography } from "@mui/material";
+import React, { useState } from "react";
+function CardComponent() {
+  const [addresses, setAddresses] = useState({ all: [], selected: "" });
   return (
-    <Card>
-      <CardMedia
-        component="img"
-        image={image}
-        alt="Image not found"
-        sx={{
-          "@media screen and (min-width:600px)": {
-            height: 200,
-          },
-        }}
-      />
-      <RemoveOutlined />
-      <Rating />
-      <Box
-        sx={{
-          "@media screen and (max-width:768px)": {
-            display: "block",
-          },
-        }}
-        width={"100%"}
-        display={"flex"}
-        alignItems={"start"}
-      ></Box>
-      <Button type="button"></Button>
-      <CardContent>
-        <Typography variant="inherit">{genre}</Typography>
-        <Typography variant="subtitle1">{title}</Typography>
-        <CircularProgress />
-        <Stack flexDirection={"column"}></Stack>
-        <Typography variant="caption">{release}</Typography>
-        <Link
-          href={`https://www.${videoLink}`}
-          target="_blank"
-          display={"block"}
+    <Box padding={1}>
+      <Grid container>
+        <Grid
+          item
+          display={"flex"}
+          justifyContent={"space-between"}
         >
-          Watch
-        </Link>
-      </CardContent>
-    </Card>
+          <Typography variant="h6" color="initial" fontWeight={""} >
+            Lorem ipsum dolor
+          </Typography>
+          <Typography variant="body1" color="initial">
+            Lorem ipsum dolor
+          </Typography>
+        </Grid>
+      </Grid>
+    </Box>
   );
 }
 
--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,40 @@
-import React from "react";
+// import React from "react";
 
-import VideosComponent from "./APIDebouncing/Videos.tsx";
+// import VideosComponent from "./APIDebouncing/Videos.tsx";
 
-function App() {
-  return <VideosComponent />;
+// function App() {
+//   return <VideosComponent />;
+// }
+// export default App;
+
+import { Box, Button, Grid, Typography } from "@mui/material";
+import React, { useState } from "react";
+function CardComponent() {
+  const [addresses, setAddresses] = useState({ all: [], selected: "" });
+  return (
+    <Box padding={1}>
+      <Grid container>
+        <Grid item display={"flex"} justifyContent={"space-between"}>
+          <Typography variant="h6" color="initial" fontWeight={""}>
+            Lorem ipsum dolor
+          </Typography>
+          <Typography variant="body1" color="initial">
+            Lorem ipsum dolor
+          </Typography>
+        </Grid>
+        {addresses.all.map((i) => {
+          return <Typography>{i} </Typography>;
+        })}
+      </Grid>
+      <Button
+        onClick={() => {
+          setAddresses({ all: [10, 20, 30, 40], selected: "1" });
+        }}
+      >
+        Click
+      </Button>
+    </Box>
+  );
 }
-export default App;
+
+export default CardComponent;

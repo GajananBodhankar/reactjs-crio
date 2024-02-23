438403069a6ad03f7b08da543397e11f926c3f25
GajananBodhankar <bodhankargajanan99@gmail.com>
2024-02-23 13:32:34 +0530

    Configured react router dom

--- src/UseOutletContext/Router.tsx
+++ src/UseOutletContext/Router.tsx
@@ -1,18 +1,27 @@
 import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import {
+  BrowserRouter,
+  Route,
+  RouterProvider,
+  Routes,
+  createBrowserRouter,
+} from "react-router-dom";
 import Parent from "./Parent.tsx";
 import ChildOne from "./ChildOne.tsx";
-
+let router = createBrowserRouter([
+  {
+    path: "/",
+    Component: Parent,
+    children: [
+      {
+        path: "/child",
+        Component: ChildOne,
+      },
+    ],
+  },
+]);
 function Router() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" Component={Parent}>
-          <Route path="/child" Component={ChildOne} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default Router;

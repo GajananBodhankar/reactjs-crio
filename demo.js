895922e9ba3bd031db4797cf65c605937ac066da
GajananBodhankar <bodhankargajanan99@gmail.com>
2024-03-11 13:52:19 +0530

    Made a change

--- demo.js
+++ demo.js
@@ -1,8 +1,26 @@
-const Obj = {
-  name: "gajanan",
-  disp: function () {
-    console.log(this.name);
-  },
-};
+function quick(start, end, arr) {
+  if (start < end) {
+    let j = partition(start, end, arr);
+    quick(start, j - 1, arr);
+    quick(j + 1, end, arr);
+  }
+  return arr;
+}
 
-console.log(JSON.stringify(Obj));
+function partition(start, end, arr) {
+  let j = start - 1,
+    pivot = arr[end];
+  for (let i = start; i < end; i++) {
+    if (arr[i] < pivot) {
+      j++;
+      tem = arr[i];
+      arr[i] = arr[j];
+      arr[j] = tem;
+    }
+  }
+  temp = arr[j + 1];
+  arr[j + 1] = arr[end];
+  arr[end] = temp;
+  return j + 1;
+}
+console.log(quick(0, 4, [5, 4, 3, 2, 1]));
--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useCallback, useEffect, useRef, useState } from "react";
 
 function App() {
-  return <div>hello I am a div</div>;
+  const [val, setVal] = useState(0);
+  const [state, setState] = useState(false);
+  const childRef = useRef<any>(null);
+  const handleChange = useCallback(() => {
+    setState((s) => !s);
+  }, [val]);
+  useEffect(() => {
+    console.log(childRef.current == handleChange);
+    childRef.current = handleChange;
+  });
+  return (
+    <div>
+      <button ref={childRef} onClick={handleChange}>
+        Change state
+      </button>
+      <p>The value is {val}</p>
+      <button onClick={() => setVal(val + 1)}>Increment</button>
+    </div>
+  );
 }
 
 export default App;

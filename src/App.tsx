06329f5ab5fc7f0bca8c329bb69df6bc10a0215a
GajananBodhankar <bodhankargajanan99@gmail.com>
2024-03-18 14:27:21 +0530

    Interview practice

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,22 @@
 import React, { useCallback, useEffect, useRef, useState } from "react";
-import IndexOne from "./InterviewQuestions/IndexOne.tsx";
-import Parent from "./Rerender/Parent.tsx";
-import Index from "./StopWatch/Index.tsx";
-import Signal from "./StopWatch/Signal.tsx";
-import Calci from "./Calculator/Calci.tsx";
-import { StyledEngineProvider } from "@mui/material";
-import Wrapper from "./HOC/Wrapper.tsx";
-
-interface Ireducer {
-  name: string;
-  age: number;
-}
-
-interface Idata {
-  data: Ireducer[];
-}
-const data: Idata = {
-  data: [],
-};
-
-function ReducerFun(state,action){
-  switch(action.type){
-    case 'push':{
-      return [...state,action.payload]
-    }
-  }
-}
 
 function App() {
+  const [val, setVal] = useState(0);
+  const [bool, setBool] = useState(false);
+  const refer = useRef<HTMLButtonElement>(null);
+  const handleChange = useCallback(() => setVal((prev) => prev + 1), [val]);
+  useEffect(() => {
+    console.log(refer.current == handleChange);
+    refer.current = handleChange;
+  });
   return (
-    <StyledEngineProvider injectFirst>
-      {/* return <Index />;
-    return <Signal />; */}
-      {/* <Calci /> */}
-      <Wrapper />
-    </StyledEngineProvider>
+    <div>
+      <button onClick={handleChange} ref={refer}>
+        Increment
+      </button>
+      <p>Value is {val}</p>
+      <button onClick={() => setBool((prev) => !prev)}>Change bool</button>
+    </div>
   );
 }
 

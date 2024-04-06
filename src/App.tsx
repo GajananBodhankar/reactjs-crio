ca98176e2db70726c5623be7230d3e5685ac7a68
GajananBodhankar <bodhankargajanan99@gmail.com>
2024-04-06 21:47:28 +0530

    Added custom debouncing in demo.html

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import React from "react";
 import StopWatchWithSetTimeout from "./StopWatch/StopWatchWithSetTimeout.tsx";
 import Index from "./StopWatch/Index.tsx";
+import ParentComponent from "./forwardRef/ParentComponent.tsx";
+import Component from "./CustomDebouncing/Component.tsx";
+import Throattling from "./CustomThroattling/Throattling.tsx";
 // import Index from "./StaleState/Index.tsx";
 
 function App() {
   // return <StopWatchWithSetTimeout />;
   // return <Index />;
-  return <Index />;
+  // return <Index />;
+  return <Component />;
+  return <Throattling />;
+  // return <ParentComponent />;
 }
 
 export default App;

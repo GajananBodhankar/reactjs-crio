bffa2c0d1b55500df8b71e4f019325b252a77e55
GajananBodhankar <bodhankargajanan99@gmail.com>
2024-04-14 20:14:58 +0530

    Donw with search filter

--- src/MultiSearchFilter/SearchFilter.tsx
+++ src/MultiSearchFilter/SearchFilter.tsx
@@ -1,14 +1,46 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./style.css";
+import axios from "axios";
 function SearchFilter() {
-  const [data, setData] = useState([10]);
+  const [data, setData] = useState([]);
   const [searchItem, setSearchItem] = useState("");
+  const [resultData, setResultData] = useState([]);
+  async function apiCall(api) {
+    if (searchItem) {
+      try {
+        let result = await axios.get(api);
+        console.log(searchItem);
+        if (result.data) {
+          setResultData(result.data.users);
+        }
+      } catch (error) {
+        alert(`${error}`);
+      }
+    } else {
+      setResultData([]);
+    }
+  }
+  useEffect(() => {
+    let timer: string | number | NodeJS.Timeout | undefined;
+    timer = setTimeout(
+      () =>
+        searchItem
+          ? apiCall(`https://dummyjson.com/users/search?q=${searchItem}`)
+          : setResultData([]),
+      400
+    );
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [searchItem]);
+
   return (
-    <div>
+    <div style={{ position: "relative" }}>
       <div className="container">
         {data.map((i, j) => (
           <span>
-            {i}{" "}
+            {i.firstName}{" "}
             <span
               onClick={() => {
                 let temp = [...data];
@@ -23,15 +55,25 @@ function SearchFilter() {
         <input
           type="text"
           value={searchItem}
-          onChange={(e) => setSearchItem(e.target.value)}
-          placeholder="Enter name here..."
-          onKeyDown={(e) => {
-            if (e.key == "Enter") {
-              setData([...data, searchItem]);
-              setSearchItem("");
-            }
+          onChange={(e) => {
+            setSearchItem(e.target.value);
           }}
+          placeholder="Enter name here..."
         />
+      </div>
+      <div className="searchResult">
+        {resultData.map((i) => (
+          <p
+            onClick={() =>
+              setData([
+                ...data,
+                { firstName: i.firstName, lastName: i.lastName },
+              ])
+            }
+          >
+            {i.firstName} {i.lastName}
+          </p>
+        ))}
       </div>
     </div>
   );
